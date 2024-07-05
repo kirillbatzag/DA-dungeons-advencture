f5ef1dcddf84c17d4004328e3926ea27b1c573f4
Kirill <kirillzagkr@gmail.com>
2024-07-05 11:12:06 +0300

    Рабочий ход
    Исправить баги и допелить!

--- player.gd
+++ player.gd
@@ -1,37 +1,22 @@
 extends CharacterBody2D
 
-# Параметры персонажа
-var move_speed = 200 # Скорость персонажа
- # Вектор скорости
+var hod = 3
 
-func _ready():
-	# Инициализация параметров при запуске сцены
-	pass
+var currPos = [0, 0]
 
-func _physics_process(delta):
-	velocity = Vector2.ZERO # Сброс скорости
-
-	# Проверка ввода пользователя и установка направления движения
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += move_speed
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= move_speed
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += move_speed
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= move_speed
-
-	# Нормализация вектора скорости
-	velocity = velocity.normalized() * move_speed
-
-	# Перемещение персонажа
-	move_and_slide()
-
-# Функция для перемещения персонажа на определённое количество шагов
-func move_steps(steps):
-	var step_count = 0
-	while step_count < steps:
-		velocity = Vector2(1, 0) * move_speed # Пример направления
-		move_and_slide()
-		step_count += 1
-		await get_tree().create_timer(1.0).timeout # Пауза между шагами
+func _input(event):
+	if event.is_action_pressed("ui_right"):
+		currPos[0] += 32
+		get_node("AnimatedSprite2D").look_at(self.position + Vector2(1,0))
+	elif event.is_action_pressed("ui_left"):
+		currPos[0] -= 32
+		get_node("AnimatedSprite2D").look_at(self.position + Vector2(-1,0))
+	elif event.is_action_pressed("ui_up"):
+		currPos[1] -= 32
+		get_node("AnimatedSprite2D").look_at(self.position + Vector2(0,-1))
+	elif event.is_action_pressed("ui_up"):
+		currPos[1] += 32
+		get_node("AnimatedSprite2D").look_at(self.position + Vector2(0,1))
+		
+	self.position = Vector2(currPos[0], currPos[1])
+	
