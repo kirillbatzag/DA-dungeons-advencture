31f38cdebb21200018414b7bd0094fabd6f68b59
Kirill <kirillzagkr@gmail.com>
2024-07-05 11:43:35 +0300

    Ебать я крутой
    !!!

--- player.gd
+++ player.gd
@@ -1,22 +1,34 @@
 extends CharacterBody2D
 
-var hod = 3
-
-var currPos = [0, 0]
+var hod = 1000
+var currPos = Vector2()
 
 func _input(event):
-	if event.is_action_pressed("ui_right"):
-		currPos[0] += 32
-		get_node("AnimatedSprite2D").look_at(self.position + Vector2(1,0))
-	elif event.is_action_pressed("ui_left"):
-		currPos[0] -= 32
-		get_node("AnimatedSprite2D").look_at(self.position + Vector2(-1,0))
-	elif event.is_action_pressed("ui_up"):
-		currPos[1] -= 32
-		get_node("AnimatedSprite2D").look_at(self.position + Vector2(0,-1))
-	elif event.is_action_pressed("ui_up"):
-		currPos[1] += 32
-		get_node("AnimatedSprite2D").look_at(self.position + Vector2(0,1))
-		
-	self.position = Vector2(currPos[0], currPos[1])
+	if hod > 0:
+		if event.is_action_pressed("ui_right"):
+			hod -= 1
+			currPos.x += 32
+			$AnimatedSprite2D.look_at(self.position + Vector2(1,0))
+		elif event.is_action_pressed("ui_left"):
+			hod -= 1
+			currPos.x -= 32
+			$AnimatedSprite2D.look_at(self.position + Vector2(-1,0))
+		elif event.is_action_pressed("ui_up"):
+			hod -= 1
+			currPos.y -= 32
+			$AnimatedSprite2D.look_at(self.position + Vector2(0,-1))
+		elif event.is_action_pressed("ui_down"):
+			hod -= 1
+			currPos.y += 32
+			$AnimatedSprite2D.look_at(self.position + Vector2(0,1))
+		$ProgressBar.value = hod
+	self.position = currPos
+
+func _ready():
+	$ProgressBar.value = hod
+
+
+func _on_get_hod_pressed():
+	if hod <= 0:
+		hod = randi() % 6 + 1
 	
