2b18572544f485ad70d89cfe0bbfbb046685f4d3
xeren12 <xeren13@mail.ru>
2024-07-07 00:51:38 +0300

    1`23
    123

--- player.gd
+++ player.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
 
-var hod = 1000
+var hod = 3
 var currPos = Vector2()
 
 func _input(event):
 	if hod > 0:
 		if event.is_action_pressed("ui_right"):
 			hod -= 1
-			currPos.x += 32
+			currPos.x += 128
 			$AnimatedSprite2D.look_at(self.position + Vector2(1,0))
 		elif event.is_action_pressed("ui_left"):
 			hod -= 1
-			currPos.x -= 32
+			currPos.x -= 128
 			$AnimatedSprite2D.look_at(self.position + Vector2(-1,0))
 		elif event.is_action_pressed("ui_up"):
 			hod -= 1
-			currPos.y -= 32
+			currPos.y -= 130
 			$AnimatedSprite2D.look_at(self.position + Vector2(0,-1))
 		elif event.is_action_pressed("ui_down"):
 			hod -= 1
-			currPos.y += 32
+			currPos.y += 130
 			$AnimatedSprite2D.look_at(self.position + Vector2(0,1))
 		$ProgressBar.value = hod
 	self.position = currPos
