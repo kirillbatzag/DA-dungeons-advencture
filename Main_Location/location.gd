a10be523751641b42ce3f1f801f1ffadaa168063
xeren12 <xeren13@mail.ru>
2024-07-07 05:18:29 +0300

    Zone
    12345

--- Main_Location/location.gd
+++ Main_Location/location.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
 
-func _on_area_2d_2_area_entered(area):
-	pass # Replace with function body.
+func _on_area_2d_area_entered(area):
+	if area is CharacterBody2D: # Проверяем, что вошедшая область - это игрок
+		print("enter zone") # Вызываем функцию перенаправления
+
+
--- player.gd
+++ player.gd
@@ -9,27 +9,35 @@ func _input(event):
 		if event.is_action_pressed("ui_right"):
 			for i in hod:
 				print(currPos)
+				await get_tree().create_timer(0.5).timeout 
 				hod -= 1
 				currPos.x += 128
 				$AnimatedSprite2D.look_at(self.position + Vector2(1,0))
+				self.position = currPos
 		elif event.is_action_pressed("ui_left"):
 			for i in hod:
 				print(currPos)
+				await get_tree().create_timer(0.5).timeout 
 				hod -= 1
 				currPos.x -= 128
 				$AnimatedSprite2D.look_at(self.position + Vector2(-1,0))
+				self.position = currPos
 		elif event.is_action_pressed("ui_up"):
 			for i in hod:
 				print(currPos)
+				await get_tree().create_timer(0.5).timeout 
 				hod -= 1
 				currPos.y -= 128
 				$AnimatedSprite2D.look_at(self.position + Vector2(0,-1))
+				self.position = currPos
 		elif event.is_action_pressed("ui_down"):
 			for i in hod:
 				print(currPos)
+				await get_tree().create_timer(0.5).timeout 
 				hod -= 1
 				currPos.y += 128
 				$AnimatedSprite2D.look_at(self.position + Vector2(0,1))
+				self.position = currPos
 		$ProgressBar.value = hod
 	self.position = currPos
 
@@ -40,5 +48,12 @@ func _ready():
 func _on_get_hod_pressed():
 	if hod <= 0:
 		hod = randi() % 6 + 1
+		
+func redirect_down():
+	hod = 1
+	currPos.y += size_hod
+	$AnimatedSprite2D.look_at(self.position + Vector2(0,1))
+	move_and_slide() # Это обновит позицию персонажа в физическом мире
+
 	
 	
